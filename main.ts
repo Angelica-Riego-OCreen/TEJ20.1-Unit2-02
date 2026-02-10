0e6be0d8972546b79b39ec5d9696ed3c302b8fe0
Angelica-Riego-OCreen <angelica.ocreen@stu.ocsb.ca>
2026-02-10 13:20:24 -0500

    Changed the title, comment, and hello world to hello mother teresa

--- main.ts
+++ main.ts
@@ -1,8 +1,8 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: XXX
- * Created on: Sep 2020
- * This program ...
+ * Created by: Angelica Riego-O'Creen
+ * Created on: Feb 2026
+ * This program runs hello Mother Teresa!
 */
 
-basic.showString('Hello, World!')
+basic.showString('Hello, Mother Teresa!')
